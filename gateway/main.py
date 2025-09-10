16c423ec08ec6f74d350b1e3f0db52909d34e5ac
Tobi <onatadetobi212@gmail.com>
2025-09-10 14:08:00 +0100

    shh

--- gateway/main.py
+++ gateway/main.py
@@ -11,9 +11,9 @@ app.include_router(router=routes)
     
 @app.get("/", status_code=status.HTTP_200_OK)
 async def home():
-    return {
-        "status": "Gateway is Alive!"
+    return {  
+        "status": "Gateway is Alive!"  
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", port=8088, reload=True)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8088, reload=True)  
